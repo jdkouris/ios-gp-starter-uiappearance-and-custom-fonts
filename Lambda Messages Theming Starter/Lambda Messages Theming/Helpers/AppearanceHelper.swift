a4e911fa830b962120793d1b39301ffdf216a42a
John Kouris <jdkouris@gmail.com>
2019-09-17 19:50:08 -0700

    Created the AppearanceHelper enum and called its setDarkAppearance method in the AppDelegate

--- Lambda Messages Theming Starter/Lambda Messages Theming/Helpers/AppearanceHelper.swift
+++ Lambda Messages Theming Starter/Lambda Messages Theming/Helpers/AppearanceHelper.swift
@@ -2,8 +2,17 @@
 //  AppearanceHelper.swift
 //  Lambda Messages Theming
 //
-//  Created by Andrew R Madsen on 1/28/19.
+//  Created by John Kouris on 9/17/19.
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum AppearanceHelper {
+    static var lambdaRed = UIColor(red: 212.0/255.0, green: 87.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    static var backgroundGray = UIColor(red: 45.0/255.0, green: 45.0/255.0, blue: 45.0/255.0, alpha: 1.0)
+    
+    static func setDarkAppearance() {
+        UINavigationBar.appearance().barTintColor = backgroundGray
+    }
+}
